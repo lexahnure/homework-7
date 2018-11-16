2721ce170fa0b3c7d6494381129203fd728088c8
Alexey <lexahnure@gmail.com>
2018-11-16 09:25:08 +0200

    deleted hw10

--- hw10/src/scripts/elementBuilder.js
+++
@@ -1,6 +0,0 @@
-const $ = require('jquery');
-
-module.exports = (tag = 'div', content = '', className = 'box') => {
-    let node = $(`<${tag}>`).addClass(className).html(content);
-    return node;
-};
