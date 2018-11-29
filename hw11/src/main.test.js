1ae802550a561a39de1b7272b17eb1e850575c88
Alexey <lexahnure@gmail.com>
2018-11-29 23:27:54 +0200

    write tests to main.js

--- hw11/src/main.test.js
+++ hw11/src/main.test.js
@@ -1,13 +1,15 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import { days, defaultProduct, money, users } from './constants';
-import { getDay, showMessage, getAdultUsers, getRandomUsers } from './main'
+import { getDay, showMessage, getAdultUsers, getRandomUsers, Product } from './main'
 
 const { expect } = chai;
 
 describe('getDay', () => {
+    const date = (new Date().getDay());
+    
     it('should return day name', () => {
-        expect(getDay()).to.be.equal(days[2]);
+        expect(getDay()).to.be.equal(days[date]);
     });
     
 });
@@ -28,7 +30,8 @@ describe('getAdultUsers', () => {
     it('should return users older than 18', () => {
         const testUsers = [{age: 20}, {age: 17}];
 
-        expect(getAdultUsers(testUsers)).to.be.an('array')
+        expect(getAdultUsers(testUsers))
+            .to.be.an('array')
             .that.includes(testUsers[0]).and
             .that.not.includes(testUsers[1]);
 
@@ -36,25 +39,68 @@ describe('getAdultUsers', () => {
 });
 
 describe('getRandomUsers', () => {
+    const testUsers = [{age: 20}, {age: 17}, {age: 21}, {age: 18}];
+    const mid = 2;
+    let fakeRand, fakeRound;
 
+    beforeEach(() => {
+        fakeRand = sinon.stub(Math, 'random');
+        fakeRound = sinon.stub(Math, 'round');
+    })
+    
+    afterEach(() => {
+        fakeRand.restore();
+        fakeRound.restore();
+    });
+    
+    
     it('should return false if no users', () => {
         expect(getRandomUsers()).to.be.false;
-
     });
     
     it('should return from 0 to 1', () => {
-        const testUsers = [{age: 20}, {age: 17}, {age: 21}, {age: 18}];
-        sinon.stub(Math, 'random').returns(0.7);
-
-        expect(getRandomUsers(testUsers)).to.be.an('array').that.includes(testUsers[0], testUsers[1]);        
-
+        fakeRand.returns(0.8);
+        fakeRound.returns(mid);
+        expect(getRandomUsers(testUsers))
+            .to.be.an('array')
+            .that.have.members(testUsers.slice(0, mid))
+            .that.not.have.members(testUsers.slice(mid, testUsers.length));
     });
 
     it('should return from 2 to 3', () => {
-        const testUsers = [{age: 20}, {age: 17}, {age: 21}, {age: 18}];
-        sinon.stub(Math, 'random').returns(0.1);
-
-        expect(getRandomUsers(testUsers)).to.be.an('array').that.includes(testUsers[2], testUsers[3]);
-
+        fakeRand.returns(0.1);
+        fakeRound.returns(mid);
+        expect(getRandomUsers(testUsers))
+            .to.be.an('array')
+            .that.have.members(testUsers.slice(mid, testUsers.length))
+            .that.not.have.members(testUsers.slice(0, mid));
     });
+});
+
+describe('Product', () => {
+    let product;
+
+    beforeEach(() => {
+        product = new Product;
+    });
+
+    it('should throw error if no value', () => {
+        expect(() => { product.setPrice() }).to.throw(Error);
+    });
+
+    it('should throw error if no value', () => {
+        const val = 15;
+
+        product.setPrice(val);
+        expect(product.value).to.equal(val);
+    });
+
+    it('should throw error if no value', () => {
+        const val = 9;
+        
+        product.setPrice(val);
+        expect(product.value).to.be.undefined;
+    });
+
+
 });
